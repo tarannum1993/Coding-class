1db1ebf680461ed2a4f9788b16cba85bf3dbfd6c
tarannum1993 <tarannum.s.pisces@gmail.com>
2021-01-20 19:17:45 +0530

    platform

--- sketch.js
+++ sketch.js
@@ -17,6 +17,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    
+    platform = new Ground(100,400,400,400)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display()
 }
